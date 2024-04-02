a85a6774a1d7d64c13c3a133662d9243b8087ad1
Mohammed El Dor <mohammedeldor@Mohammeds-MacBook-Air.local>
2024-04-02 16:12:37 +0200

    before training detectron2

--- Detectron2/train_detectron2.py
+++ Detectron2/train_detectron2.py
@@ -1,4 +1,5 @@
 import os
+import torch
 from detectron2 import model_zoo
 from detectron2.data.datasets import register_coco_instances
 from detectron2.config import get_cfg, CfgNode
@@ -9,9 +10,9 @@ import itertools
 
 NUM_WORKERS = 2
 # This is the real "batch size" commonly known to deep learning people
-IMS_PER_BATCH = 8
+IMS_PER_BATCH = 4
 # The "RoIHead batch size". 128 is faster, and good enough for this dataset (default: 512)
-BATCH_SIZE_PER_IMAGE = 128
+BATCH_SIZE_PER_IMAGE = 64
 NUM_CLASSES = 1 
 NUMBER_OF_TRIALS = 100
 MAX_ITER = 10
@@ -45,7 +46,7 @@ Augmentation_cfg = {
 
 def register_data():
     register_coco_instances("my_dataset_train", {}, Data_cfg["Coco_labels_train_dir"], os.path.join(Data_cfg["cropped_Images_dir"], "Train"))
-    register_coco_instances("my_dataset_test", {}, Data_cfg["Coco_labels_test_dir"], os.path.join(Data_cfg["cropped_Images_dir"], "Test"))
+    # register_coco_instances("my_dataset_test", {}, Data_cfg["Coco_labels_test_dir"], os.path.join(Data_cfg["cropped_Images_dir"], "Test"))
     register_coco_instances("my_dataset_val", {}, Data_cfg["Coco_labels_val_dir"], os.path.join(Data_cfg["cropped_Images_dir"], "Val"))
     
 def train(backbone, rpn_bbox_reg_loss, roi_bbox_reg_loss, retinanet_bbox_reg_loss, momentum,
@@ -56,26 +57,26 @@ def train(backbone, rpn_bbox_reg_loss, roi_bbox_reg_loss, retinanet_bbox_reg_los
     # now we have some configurations that we want to fix and not tune
     # this is to unfreeze layeras in the model training, by default the first 2 layers are freezed
     cfg.merge_from_file(model_zoo.get_config_file(backbone))
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(backbone)
     cfg.DATASETS.TRAIN = ("my_dataset_train",)
-    cfg.DATASETS.TEST = ("my_dataset_test",)
-    cfg.MODEL.BACKBONE.FREEZE_AT = 2
+    cfg.DATASETS.TEST = ("my_dataset_val",)
+    cfg.DATALOADER.NUM_WORKERS = num_workers
+    cfg.MODEL.BACKBONE.FREEZE_AT = 0
     cfg.SOLVER.CHECKPOINT_PERIOD = CHECKPOINT_PERIOD
     cfg.SOLVER.MAX_ITER = MAX_ITER
-    cfg.SOLVER.MOMENTUM = momentum #Detectron2_cfg["Momentum"]
+    cfg.SOLVER.MOMENTUM = momentum 
     # cfg.TEST.EVAL_PERIOD = 20
     cfg.SOLVER.IMS_PER_BATCH = ims_per_batch  # This is the real "batch size" commonly known to deep learning people
-    cfg.SOLVER.BASE_LR = learning_rate# Detectron2_cfg["base_lr"]  # pick a good LR
+    cfg.SOLVER.BASE_LR = learning_rate # Detectron2_cfg["base_lr"]  # pick a good LR
     cfg.SOLVER.STEPS = []        # do not decay learning rate
     cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = batch_size_per_image  # The "RoIHead batch size". 128 is faster, and good enough for this dataset (default: 512)
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = num_classes  
-    # this parameter is from detectron_conf
-    cfg.DATALOADER.NUM_WORKERS = num_workers
     # image normalization, they might give errors
-    cfg.MODEL.PIXEL_MEAN = [26.9]
-    cfg.MODEL.PIXEL_STD = [34.4]
+    cfg.MODEL.PIXEL_MEAN = [26.9, 26.9, 26.9]
+    cfg.MODEL.PIXEL_STD = [34.4, 34.4, 34.4]
     # for grayscale images
     # if it gives error just remove it and make mean and std 3 values that are equal [26.9, 26.9, 26.9], [34.4, 34.4, 34.4]
-    cfg.INPUT.FORMAT = "L"
+    # cfg.INPUT.FORMAT = "L"
     cfg.MODEL.RPN.BBOX_REG_LOSS_TYPE = rpn_bbox_reg_loss
     cfg.MODEL.ROI_BOX_HEAD.BBOX_REG_LOSS_TYPE = roi_bbox_reg_loss
     cfg.MODEL.RPN.BBOX_REG_LOSS_TYPE = retinanet_bbox_reg_loss
@@ -83,6 +84,9 @@ def train(backbone, rpn_bbox_reg_loss, roi_bbox_reg_loss, retinanet_bbox_reg_los
     trainer = CustomTrainer(cfg)
     trainer.resume_or_load(resume=False)
     trainer.train()
+    del trainer
+    torch.cuda.empty_cache()
+
 
 hyperparameters = {
     'backbone': ["COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml", 
